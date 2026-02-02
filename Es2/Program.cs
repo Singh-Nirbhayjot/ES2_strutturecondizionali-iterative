9846c307b1735bf54b0822988d4ccda3a356a62d
Singh-Nirbhayjot <15320992@itisrossi.vi.it>
2026-02-02 18:32:33 +0100

    Aggiungere i file di progetto.

---
+++ Es2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Es2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci il primo numero: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Inserisci il secondo numero: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int somma1 = 0;
+            int somma2 = 0;
+
+            for (int i = 1; i <= a/2; i++)
+            {
+                if (a % i == 0)
+                    somma1 += i;
+            }
+
+            for (int i = 1; i <= b/2; i++)
+            {
+                if (b % i == 0)
+                    somma2 += i;
+            }
+
+            if (somma1 == b && somma2 == a)
+                Console.WriteLine("I numeri sono amici.");
+            else
+                Console.WriteLine("I numeri non sono amici.");
+        }
+    }
+}
---
+++ Es3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Es3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci un numero: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write(n);
+
+            while (n != 1)
+            {
+                if (n % 2 == 0)
+                    n = n / 2;
+                else
+                    n = n * 3 + 1;
+
+                Console.Write($"->{n}");
+            }
+        }
+    }
+}
