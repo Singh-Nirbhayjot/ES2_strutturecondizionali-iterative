63bd90553f297e28c3a7e81deacc0d989716371e
Singh-Nirbhayjot <15320992@itisrossi.vi.it>
2026-02-03 09:24:02 +0100

    Aggiunto ES4

---
+++ Es1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Es1
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Inserisci il codice di transazione: ");
+            int codice = int.Parse(Console.ReadLine());
+
+            int c = codice;
+            int cifre = 0;
+
+            while (c > 0)
+            {
+                cifre++;
+                c /= 10;
+            }
+
+            c = codice;
+            int somma = 0;
+
+            while (c > 0)
+            {
+                int cifra = c % 10;
+                somma += (int)Math.Pow(cifra, cifre);
+                c /= 10;
+            }
+
+            if (somma == codice)
+                Console.WriteLine("Codice valido");
+            else
+                Console.WriteLine("Codice non valido");
+        }
+    }
+}
---
+++ Es4/Es4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+
+namespace Es4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Write("Inserisci numero:");
+            int.TryParse(ReadLine(), out n);
+            for (int i = 1; i <= n; i++)
+            {
+                for (int z = 1; z <= n - i; z++)
+                {
+                    Write(" ");
+                }
+                    for (int j = 1; j <= i; j++)
+                    {
+                        Write(j);
+                    }
+                    for (int j = i - 1; j >= 1; j--)
+                    {
+                        Write(j);
+                    }
+                    WriteLine();
+            }
+
+        }
+    }
+}
